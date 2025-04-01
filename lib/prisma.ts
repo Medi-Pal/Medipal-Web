398aa6bc47ae7fc501fda2cb1dbd4120836c6c63
brand7noronha <153798314+brand7noronha@users.noreply.github.com>
2025-04-01 18:42:35 +0530

    Revert "Prisma schema"

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,10 @@
 // setting up Prisma Client before querying our Database with Prisma ORM
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma = new PrismaClient()
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
